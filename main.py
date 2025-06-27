70a6d9ec633744f0cca3e179646e65a8bbc7ee0a
Pedromrls <pedromrls6@gmail.com>
2025-06-27 12:53:40 -0400

    in the content field was still using prior logic

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main ():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1],
+        contents=user_prompt,
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
