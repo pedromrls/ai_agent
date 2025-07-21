3af7c13f87993f8c449b24d76ded621503343be7
Pedromrls <pedromrls6@gmail.com>
2025-07-21 14:01:04 -0400

    Added the verbose flag to the cli tool and created separate functions for each individual process

--- main.py
+++ main.py
@@ -20,31 +20,40 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-def main ():
-    args = sys.argv[1:]
 
+def get_args():
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-        ]
-    client = genai.Client(api_key=api_key)
-    generate_content(client, messages)
+    return args
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: \n{response.text}")
+
+def main():
+    args = get_args()
+    verbose = "--verbose" in sys.argv
+    user_prompt = " ".join(args)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+    client = genai.Client(api_key=api_key)
+    generate_content(client, messages, verbose)
+
 
 if __name__ == "__main__":
     main()
