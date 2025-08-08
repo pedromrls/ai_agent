d4b673e057e1d7c9153928fbeac4b84f1c28641d
Pedromrls <pedromrls6@gmail.com>
2025-08-08 14:05:50 -0400

    feat: Add file writing functionality with path validation.

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,2 +1,21 @@
+import os
+
+
 def write_file(working_directory, file_path, content):
-    pass
+    abs_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_dir):
+        try:
+            os.makedirs(abs_dir)
+        except Exception as e:
+            return f'Error reading file "{file_path}": {e}'
+    try:
+        with open(target_file, 'w') as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f'Error writing to file: {file_path}: {e}'
--- tests.py
+++ tests.py
@@ -1,18 +1,17 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print(result)
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(f'Test 1: {result}')
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print(result)
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(f'Test 2: {result}')
 
-    result = get_file_content("calculator", "/bin/cat")
-    print(result)
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(f'Test 3: {result}')
 
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print(result)
 if __name__ == "__main__":
     test()
+
 
